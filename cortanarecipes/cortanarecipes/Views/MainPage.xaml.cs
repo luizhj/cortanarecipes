88daf30631bf0623168482894b44359b4771ef7f
luizhj <luizhj@gmail.com>
2018-11-05 15:16:53 -0200

    inheritance class view correction

--- cortanarecipes/cortanarecipes/Views/MainPage.xaml.cs
+++ cortanarecipes/cortanarecipes/Views/MainPage.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using cortanarecipes.ViewModels;
 using Xamarin.Forms.Xaml;
 
-using cortanarecipes.ViewModels;
-
-namespace cortanarecipes.Views : BasePage
+namespace cortanarecipes.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : BasePage
     {
         MainViewModel ViewModel = new MainViewModel();
 
--- cortanarecipes/cortanarecipes/Views/RecipePage.xaml.cs
+++ cortanarecipes/cortanarecipes/Views/RecipePage.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using cortanarecipes.ViewModels;
 using Xamarin.Forms.Xaml;
 
-using cortanarecipes.ViewModels;
-
-namespace cortanarecipes.Views : BasePage
+namespace cortanarecipes.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class RecipePage : ContentPage
+    public partial class RecipePage : BasePage
     {
         RecipeViewModel ViewModel = new RecipeViewModel();
 
