f1e2cb4427fc705fec0018b3389e191493a153e4
luizhj <luizhj@gmail.com>
2019-04-10 08:41:38 -0300

    removido text to speech. added appcenter integration

--- cortanarecipes/cortanarecipes.Android/MainActivity.cs
+++ cortanarecipes/cortanarecipes.Android/MainActivity.cs
@@ -21,6 +21,7 @@ namespace cortanarecipes.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
 
